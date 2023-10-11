6011adbed117ed4da342a2779798e7b64f790627
KDex dev <146705099+kdexdev@users.noreply.github.com>
2023-10-11 07:59:15 +0300

    Installed user management packages

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,6 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
     Symfonycasts\TailwindBundle\SymfonycastsTailwindBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
