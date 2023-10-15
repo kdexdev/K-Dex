1f3e80c1878aed8cf4d87c0fbe16b5865dcab675
KDex dev <146705099+kdexdev@users.noreply.github.com>
2023-10-15 19:11:15 +0300

    a failed attempt at getting visitation updating

--- src/EventListener/AuthentificationListener.php
+++ src/EventListener/AuthentificationListener.php
@@ -3,35 +3,37 @@
 namespace App\EventListener;
 
 use App\Entity\User;
-use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityManager;
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
 use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
 
 class AuthenticationListener
 {
+    protected $entityManager;
+
+    public function __construct(EntityManager $entityManager){
+        $this->entityManager = $entityManager;
+    }
+
     /**
      * Updates the database for the latest time the user has been active on the database
      *
      * @param InteractiveLoginEvent $event The event object.
      * @return void
      */
-
     #[AsEventListener(
         event: InteractiveLoginEvent::class,
         method: 'onInteractiveLogin'
     )]
-    public function onInteractiveLogin(
-        InteractiveLoginEvent $event,
-        EntityManagerInterface $entityManager
-    ): void {
+    public function onInteractiveLogin(InteractiveLoginEvent $event): void {
         $user = $event->getAuthenticationToken()->getUser();
         if ($user instanceof User) {
             // Update the last time user visited the website
             $user->setLastVisitedAt(new \DateTimeImmutable());
 
             // Update the database entry
-            $entityManager->persist($user);
-            $entityManager->flush();
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
         }
     }
 }
